48c4e6fe20547cf1531a6f592638c905134d22ab
Membri <membri_2@hotmail.com>
2019-12-15 21:47:58 +0100

    Dataseeder ajustado para generacion de datos

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -41,12 +41,12 @@ class DatabaseSeeder extends Seeder
                 factory(\App\Student::class, 1)->create(['user_id' => $u->id]);
             });
 
-        factory(\App\User::class, 50)->create()
+        factory(\App\User::class, 5)->create()
             ->each(function (\App\User $u) {
                 factory(\App\Student::class, 1)->create(['user_id' => $u->id]);
             });
 
-        factory(\App\User::class, 10)->create()
+        factory(\App\User::class, 5)->create()
             ->each(function (\App\User $u) {
                 factory(\App\Student::class, 1)->create(['user_id' => $u->id]);
                 factory(\App\Teacher::class, 1)->create(['user_id' => $u->id]);
@@ -55,9 +55,14 @@ class DatabaseSeeder extends Seeder
         factory(\App\Level::class, 1)->create(['name' => 'Beginner']);
         factory(\App\Level::class, 1)->create(['name' => 'Intermediate']);
         factory(\App\Level::class, 1)->create(['name' => 'Advanced']);
-        factory(\App\Category::class, 5)->create();
+        factory(\App\Category::class, 1)->create(['name' => 'TaiJi']);
+        factory(\App\Category::class, 1)->create(['name' => 'Sanda']);
+        factory(\App\Category::class, 1)->create(['name' => 'Wu-shu']);
+        factory(\App\Category::class, 1)->create(['name' => 'Cuchillo']);
+        factory(\App\Category::class, 1)->create(['name' => 'Mano-vacia']);
+        factory(\App\Category::class, 1)->create(['name' => 'Kali']);
 
-        factory(\App\Course::class, 50)
+        factory(\App\Course::class, 5)
             ->create()
             ->each(function (\App\Course $c) {
                 $c->goals()->saveMany(factory(\App\Goal::class, 2)->create());
